3a17e52fa3dbf4ec738fa4990e6014b5c5c87a8e
Maarten <maartenzam@gmail.com>
2021-10-27 11:52:03 +0200

    Initial commit: line chart

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,18 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <LineChart />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import LineChart from "./components/LineChart.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    LineChart,
+  },
+};
 </script>
 
 <style>
@@ -24,5 +23,6 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  width: 100%;
 }
 </style>
---
+++ src/components/LineChart.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="viz"></div>
+</template>
+
+<script>
+import { default as vegaEmbed } from "vega-embed";
+import lineSpec from "../assets/line.vl.json";
+import fieldkitData from "../assets/fieldkitData.json";
+
+lineSpec.data = { values: fieldkitData.data };
+
+export default {
+  name: "LineChart",
+  props: {
+    msg: String,
+  },
+  mounted: function () {
+    vegaEmbed("#viz", lineSpec, {
+      renderer: "svg",
+      tooltip: { offsetX: -50, offsetY: 50 },
+      actions: { source: false, editor: false, compiled: false },
+    });
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+@font-face {
+  font-family: Avenir Light;
+  src: url("../assets/fonts/Avenir/OpenType CFF Pro/AvenirLTPro-Light.otf");
+}
+#viz {
+  width: 100%;
+}
+.mark-text.role-axis-label {
+  font-family: "Avenir Light", sans-serif;
+}
+</style>
