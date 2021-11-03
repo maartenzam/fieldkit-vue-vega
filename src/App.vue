70928b5363f53f4cc8b2057b89561ca892c0712b
Maarten <maartenzam@gmail.com>
2021-11-03 17:07:52 +0100

    Basic double line chart

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="charts">
     <div class="chart">
+      <DoubleLineChart />
+    </div>
+    <div class="chart">
+      <Scrubber />
+    </div>
+    <div class="chart">
       <RangeChart />
     </div>
     <div class="chart">
@@ -13,9 +19,11 @@
 </template>
 
 <script>
+import Scrubber from "./components/Scrubber.vue";
 import LineChart from "./components/LineChart.vue";
 import Histogram from "./components/Histogram.vue";
 import RangeChart from "./components/RangeChart.vue";
+import DoubleLineChart from "./components/DoubleLineChart.vue";
 
 export default {
   name: "App",
@@ -23,6 +31,8 @@ export default {
     LineChart,
     Histogram,
     RangeChart,
+    Scrubber,
+    DoubleLineChart,
   },
 };
 </script>
---
+++ src/components/DoubleLineChart.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="viz doublelinechart"></div>
+</template>
+
+<script>
+import { default as vegaEmbed } from "vega-embed";
+import doublelineSpec from "../assets/doubleline.vl.json";
+import fieldkitData from "../assets/fieldkitData.json";
+import fieldkitWindSpeedData from "../assets/fieldkitWindSpeedData.json";
+
+doublelineSpec.layer[0].data = { values: fieldkitData.data };
+doublelineSpec.layer[1].data = { values: fieldkitData.data };
+doublelineSpec.layer[2].data = { values: fieldkitWindSpeedData.data };
+
+export default {
+  name: "DoubleLineChart",
+  mounted: function () {
+    vegaEmbed(".doublelinechart", doublelineSpec, {
+      renderer: "svg",
+      tooltip: { offsetX: -50, offsetY: 50 },
+      actions: { source: false, editor: false, compiled: false },
+    }); /*.then((result) => {
+      console.log(result.vgSpec);
+    });*/
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+@font-face {
+  font-family: Avenir Light;
+  src: url("../assets/fonts/Avenir/OpenType CFF Pro/AvenirLTPro-Light.otf");
+}
+.viz {
+  width: 100%;
+}
+.mark-text.role-axis-label {
+  font-family: "Avenir Light", sans-serif;
+}
+</style>
