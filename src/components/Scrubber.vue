a0cff4af45c75bf368c626930abcd07bd48a22c3
Maarten <maartenzam@gmail.com>
2021-11-06 00:24:54 +0100

    Signals in and out of scrubber

--- src/components/Scrubber.vue
+++ src/components/Scrubber.vue
@@ -1,5 +1,8 @@
 <template>
-  <div class="viz scrubber"></div>
+  <div>
+    <button v-on:click="pickRange">Pick date range</button>
+    <div class="viz scrubber"></div>
+  </div>
 </template>
 
 <script>
@@ -11,16 +14,42 @@ scrubberSpec.data = { values: fieldkitData.data };
 
 export default {
   name: "Scrubber",
+  data() {
+    return { dateRange: { time: [1630044000000, 1631577600000] } };
+  },
   mounted: function () {
     vegaEmbed(".scrubber", scrubberSpec, {
       renderer: "svg",
-      //tooltip: { offsetX: -50, offsetY: 50 },
       actions: { source: false, editor: false, compiled: false },
     }).then((result) => {
       result.view.addSignalListener("brush", function (name, value) {
         console.log(value);
       });
     });
+  },
+  methods: {
+    pickRange: function () {
+      vegaEmbed(".scrubber", scrubberSpec, {
+        renderer: "svg",
+        actions: { source: false, editor: false, compiled: false },
+      }).then((result) => {
+        result.view.addSignalListener("brush", function (_, value) {
+          console.log(value);
+        });
+        result.view.signal("brush_tuple", {
+          unit: "layer_0",
+          fields: [
+            {
+              field: "time",
+              channel: "x",
+              type: "R",
+            },
+          ],
+          values: [[1629956332434, 1630344275971]],
+        });
+        result.view.runAsync();
+      });
+    },
   },
 };
 </script>
