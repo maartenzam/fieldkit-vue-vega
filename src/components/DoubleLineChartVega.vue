d0ba3991ad0e5a798471527261d46c4156b7a16c
Maarten <maartenzam@gmail.com>
2022-03-14 18:15:37 +0100

    Colours and legends

--- src/components/DoubleLineChartVega.vue
+++ src/components/DoubleLineChartVega.vue
@@ -18,38 +18,149 @@ fieldkitTemperatureData.data.forEach(item => {
 doublelineVegaSpec.data[0].values = fieldkitHumidityData.data
 doublelineVegaSpec.data[1].values = fieldkitTemperatureData.data
 
-const colorLeft = "#5736FF";
+/*const colorLeft = "#5736FF";
 const colorRight = "#3fab29";
 
 doublelineVegaSpec.marks[0].marks[1].encode.enter.stroke.value = colorLeft
-doublelineVegaSpec.marks[0].marks[2].encode.enter.fill.value = colorLeft
+doublelineVegaSpec.marks[0].marks[3].encode.enter.fill.value = colorLeft
 doublelineVegaSpec.marks[1].marks[1].encode.enter.stroke.value = colorRight
-doublelineVegaSpec.marks[1].marks[2].encode.enter.fill.value = colorRight
+doublelineVegaSpec.marks[1].marks[2].encode.enter.fill.value = colorRight*/
 
 doublelineVegaSpec.axes[1].title = "Humidity (%)";
 doublelineVegaSpec.axes[2].title = "Temperature (°F)";
 
-//import chartConfig from "../assets/chartConfig.json";
+let thresholdsLeft = [
+  {
+    value: 20,
+    label: "Very dry",
+    color: "#f1eef6",
+  },
+  {
+    value: 40,
+    label: "Dry",
+    color: "#bdc9e1",
+  },
+  {
+    value: 60,
+    label: "Wet",
+    color: "#74a9cf",
+  },
+  {
+    value: 80,
+    label: "Very wet",
+    color: "#2b8cbe",
+  },
+  {
+    value: 100,
+    label: "Very, very wet",
+    color: "#045a8d",
+  },
+];
 
-//doublelineSpec.config = chartConfig;
+const thresholdsLeftTransforms = thresholdsLeft.map((d,i) => {
+        return {"type": "formula", "expr": "datum.value <= " + d.value  + " ? datum.value : null", "as": "wet" + i}  
+})
 
-/*
+doublelineVegaSpec.data[0].transform = thresholdsLeftTransforms
 
-doublelineSpec.layer[0].data = { values: fieldkitHumidityData.data };
-doublelineSpec.layer[0].encoding.y.title = "Humidity (%)";
-doublelineSpec.layer[0].layer[1].mark.color = colorLeft;
-doublelineSpec.layer[0].layer[2].mark.color = colorLeft;
-doublelineSpec.layer[0].encoding.y.axis.titleColor = colorLeft;
-doublelineSpec.layer[0].encoding.y.axis.gridColor = colorLeft;
-doublelineSpec.layer[0].encoding.y.axis.labelColor = colorLeft;
+let thresholdsRight = [
+  {
+    value: -40,
+    label: "Very cold",
+    color: "#fef0d9",
+  },
+  {
+    value: 0,
+    label: "Cold",
+    color: "#fdcc8a",
+  },
+  {
+    value: 5,
+    label: "Regular",
+    color: "#fc8d59",
+  },
+  {
+    value: 10,
+    label: "Warm",
+    color: "#e34a33",
+  },
+  {
+    value: 40,
+    label: "Very warm",
+    color: "#b30000",
+  },
+];
 
-doublelineSpec.layer[1].data = { values: fieldkitTemperatureData.data };
-doublelineSpec.layer[1].encoding.y.title = "Temperature (°F)";
-doublelineSpec.layer[1].layer[1].mark.color = colorRight;
-doublelineSpec.layer[1].layer[2].mark.color = colorRight;
-doublelineSpec.layer[1].encoding.y.axis.titleColor = colorRight;
-doublelineSpec.layer[1].encoding.y.axis.gridColor = colorRight;
-doublelineSpec.layer[1].encoding.y.axis.labelColor = colorRight;*/
+const thresholdsRightTransforms = thresholdsRight.map((d,i) => {
+        return {"type": "formula", "expr": "datum.value <= " + d.value  + " ? datum.value : null", "as": "warm" + i}  
+})
+
+doublelineVegaSpec.data[1].transform = thresholdsRightTransforms
+
+const thresholdsLeftMarks = thresholdsLeft.map((d,i) => {
+    return{
+        "type": "line",
+          "from": { "data": "table1" },
+          "encode": {
+            "enter": {
+              "interpolate": { "value": "cardinal" },
+              "tension": { "value": 0.9 },
+              "x": { "scale": "x", "field": "time" },
+              "y": { "scale": "y", "field": "wet" + i},
+              "stroke": { "value": d.color },
+              "strokeWidth": { "value": 2 },
+              "defined": { "signal": "isValid(datum.wet" + i + ")" }
+            },
+            "update": {
+              "strokeOpacity": {
+                "signal": "hover.name == 'Humidity' ? 1 : 0.1"
+              }
+            }
+          }
+        }
+})
+doublelineVegaSpec.marks[0].marks = [doublelineVegaSpec.marks[0].marks[0]].concat(thresholdsLeftMarks.reverse()).concat(doublelineVegaSpec.marks[0].marks[1])
+
+const thresholdsRightMarks = thresholdsRight.map((d,i) => {
+    return{
+        "type": "line",
+          "from": { "data": "table2" },
+          "encode": {
+            "enter": {
+              "interpolate": { "value": "cardinal" },
+              "tension": { "value": 0.9 },
+              "x": { "scale": "x", "field": "time" },
+              "y": { "scale": "y2", "field": "warm" + i},
+              "stroke": { "value": d.color },
+              "strokeWidth": { "value": 2 },
+              "defined": { "signal": "isValid(datum.warm" + i + ")" }
+            },
+            "update": {
+              "strokeOpacity": {
+                "signal": "hover.name == 'Temperature' ? 1 : 0.1"
+              }
+            }
+          }
+        }
+})
+doublelineVegaSpec.marks[1].marks = [doublelineVegaSpec.marks[1].marks[0]].concat(thresholdsRightMarks.reverse()).concat(doublelineVegaSpec.marks[1].marks[1])
+
+doublelineVegaSpec.scales = doublelineVegaSpec.scales.concat([
+    {
+      "name": "colorLeft",
+      "type": "ordinal",
+      "domain": thresholdsLeft.map(d => d.label),
+      "range": thresholdsLeft.map(d => d.color)
+    },
+    {
+      "name": "colorRight",
+      "type": "ordinal",
+      "domain": thresholdsRight.map(d => d.label),
+      "range": thresholdsRight.map(d => d.color)
+    }
+    ]
+)
+console.log(doublelineVegaSpec.scales)
 
 export default {
   name: "DoubleLineChartVega",
